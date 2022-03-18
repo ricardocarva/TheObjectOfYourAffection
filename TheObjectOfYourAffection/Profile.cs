18c4baafd1205faa502981f52b78e5e254ce993a
ricardocarva <ricardofecarva@gmail.com>
2022-03-18 12:26:18 -0400

    Add project files.

---
+++ TheObjectOfYourAffection/Profile.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheObjectOfYourAffection
+{
+    class Profile
+    {
+        //Field
+        private string name;
+        private int age;
+        private string city;
+        private string country;
+        private string pronouns;
+        private string[] hobbies;
+
+        public int Age
+        {
+            get { return age; }
+            private set
+            {
+                if (value >= 18) 
+                    age = value;
+                else
+                {
+                    throw new Exception($"You must be over 18 yo to use this application!");
+
+                }
+            }
+        }
+
+        //Constructor
+        public Profile(string city, string country, string name = "n/a", int age = 18, string pronouns = "they/them")
+        {
+            this.name = name;
+            this.Age = age;
+            this.city = city;
+            this.country = country;
+            this.pronouns = pronouns;
+            this.hobbies = new string[0];
+        }
+        public Profile()
+        {
+            Console.Write("Enter Name: ");
+            string name = Console.ReadLine();
+            this.name = name;
+
+            Console.Write("Enter Age: ");
+            string ageString = Console.ReadLine();
+            
+            while (!Int32.TryParse(ageString, out age))
+            {
+                Console.Write("Invalid Age. Please try again.\r\n");
+                Console.Write("Enter Age: ");
+                ageString = Console.ReadLine();
+            }
+
+            this.Age = age;
+
+            Console.Write("Enter City: ");
+            string city = Console.ReadLine();
+            this.city = city;
+
+
+
+            Console.Write("Enter Country: ");
+            string country = Console.ReadLine();
+            this.country = country;
+
+            Console.Write("Enter Pronouns: ");
+            string pronouns = Console.ReadLine();
+            this.pronouns = pronouns;
+
+            this.hobbies = new string[0];
+        }
+        //Methods
+        public string ViewProfile()
+        {
+            string profile = $"Name: {name}\r\nAge: {age}\r\nCity: {city}\r\nCountry: {country}\r\nPronouns: {pronouns}";
+
+            if (hobbies.Length != 0)
+            {
+                profile += "\r\nHobbies:";
+                foreach (string hobby in hobbies)
+                {
+                    profile += $"\n- {hobby}";
+                }
+
+            }
+            return profile;
+        }
+        public void SetHobbies(string[] hobbies)
+        {
+            this.hobbies = hobbies;
+        }
+
+        public void SetHobbies()
+        {
+            Console.Write("How many hobbies would you like to add: ");
+            string numHobbiesString = Console.ReadLine();
+            int numHobbies;
+            while (!Int32.TryParse(numHobbiesString, out numHobbies))
+            {
+                Console.Write("Invalid number of hobbies. Please try again.\r\n");
+                Console.Write("How many hobbies would you like to add:");
+                numHobbiesString = Console.ReadLine();
+
+            }
+            for (int i=0; i< numHobbies; i++)
+            {
+                Console.Write($"Enter Hobby Number {i + 1}: ");
+                string hobby = Console.ReadLine();
+                this.hobbies[i] = hobby;
+            }
+            
+        }
+    }
+}
---
+++ TheObjectOfYourAffection/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheObjectOfYourAffection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+            This program generates a profile for a new match-making service.
+            It focus on the creation of a new user's profile.
+
+            The Profile should store the following information:
+
+                User’s name
+                User’s age
+                User’s city
+                User’s country
+                User’s pronouns
+                User’s hobbies
+                And this is how users should be able to interact with their own profiles:
+
+                Create a new profile with some information
+                Add hobbies
+                View profile
+             
+                        Author: Ricardo Carvalheira
+
+            */
+
+            Profile NewUser = new Profile();
+            string result3 = NewUser.ViewProfile();
+            NewUser.SetHobbies();
+
+            //Profile sam = new Profile("New York", "USA", "Sam Drakkila", 18, "He/Him");
+            //string result = sam.ViewProfile();
+            //sam.SetHobbies(new string[] { "listening to audiobooks and podcast", "playing rec sports like bowling and kickball", "writing a speculative fiction novel", "reading advice columns" });
+            //result = sam.ViewProfile();
+            //Console.WriteLine($"Profile 1: {result}");
+
+            //Profile Kelly = new Profile("New York", "USA");
+            //string result2 = Kelly.ViewProfile();
+            //Console.WriteLine($"\nProfile 2: {result2}");
+
+            Console.WriteLine($"\nNew Profile: {result3}");
+
+
+            Console.WriteLine("\r\n     Press Enter to close tab...");
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
